007e9d87dc974a9ac975119f5f6bc418160a6ac3
Amit Saha <amitsaha.in@gmail.com>
2021-09-24 16:19:00 +1000

    update

--- public/server.go
+++ public/server.go
@@ -4,16 +4,23 @@ import (
 	"embed"
 	"log"
 	"net/http"
+	"os"
 )
 
-//go:embed about categories css images posts
+//go:embed posts code
 //go:embed index.html index.xml sitemap.xml
+//go:embed categories css images
 var siteData embed.FS
 
 func main() {
+	listenAddr := ":80"
+	if len(os.Getenv("LISTEN_ADDR")) != 0 {
+		listenAddr = os.Getenv("LISTEN_ADDR")
+
+	}
 	mux := http.NewServeMux()
 	staticFileServer := http.FileServer(http.FS(siteData))
 	mux.Handle("/", staticFileServer)
 
-	log.Fatal(http.ListenAndServe(":80", mux))
+	log.Fatal(http.ListenAndServe(listenAddr, mux))
 }
