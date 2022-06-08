e00a7674cc8f9d0e13d26c64eaf2dd4d22e556a6
Amit Saha <mail@echorand.me>
2022-06-08 12:39:28 +1000

    add errata

--- content/errata.md
+++ content/errata.md
@@ -1,19 +1,20 @@
 ---
 title: "Errata"
-date: 2022-06-06
+date: 2022-06-06T08:27:41+11:00
+
 ---
 
 If you find an error, please let me know: mail@practicalgobook.net
 
 ## Chapter 1
 
-### Page 5 (Electronic and Print)
+*Page 5 (Electronic and Print)*
 
-Current `greetUser()` function is missing a closing paren in the arguments. 
+Current `greetUser()` function is missing a closing paren in the arguments. The fixed version:
 
 ```go
 func greetUser(c config, name string, w io.Writer) {
-....
+        // function body remains same
 }
 ```
 
--- public/server.go
+++ public/server.go
@@ -5,17 +5,13 @@ import (
 	"log"
 	"net/http"
 	"os"
-
-	"github.com/facebookgo/grace/gracehttp"
-	"github.com/slayer/autorestart"
 )
 
-//go:embed buy categories code css images posts support tags toc
-//go:embed book_cover.jpg go.mod index.html index.xml sitemap.xml
+//go:embed buy categories code css errata images posts support tags toc  
+//go:embed book_cover.jpg go.mod go.sum index.html index.xml server server.go sitemap.xml  
 var siteData embed.FS
 
 func main() {
-	logger := log.New(os.Stdout, "INFO: ", log.Lshortfile)
 	listenAddr := ":8080"
 	if len(os.Getenv("LISTEN_ADDR")) != 0 {
 		listenAddr = os.Getenv("LISTEN_ADDR")
@@ -25,20 +21,5 @@ func main() {
 	staticFileServer := http.FileServer(http.FS(siteData))
 	mux.Handle("/", staticFileServer)
 
-	srv := http.Server{
-		Addr:    listenAddr,
-		Handler: mux,
-	}
-
-	autorestart.RestartFunc = autorestart.SendSIGUSR2
-	restart := autorestart.GetNotifier()
-	go func() {
-		<-restart
-		logger.Printf("Detected change in binary. Restarting.")
-	}()
-
-	autorestart.StartWatcher()
-
-	gracehttp.SetLogger(logger)
-	logger.Fatalf("Server terminating: %v", gracehttp.Serve(&srv))
+	log.Fatal(http.ListenAndServe(listenAddr, mux))
 }
